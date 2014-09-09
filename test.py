e1725001bdfac29f0a4aceaa0b2b4de8d82c3a25
Diego <dascaniosantos@gmail.com>
2014-09-09 20:46:54 -0300

    blabla

---
+++ test.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+#Resolvendo o problema do Ano Bissexto - http://dojopuzzles.com/problemas/exibe/ano-bissexto/
+
+#import unittest
+#class TestAnoBissexto(unittest.TestCase):
+    
+def bissexto(ano):
+    # verifica se o ano é multiplo de 4 e não divisível por 100
+    if ano % 4 == 0 and ano % 100 != 0:
+        return True
+    # verifica se divisivel por 400
+    elif ano % 400 == 0:
+        return True
+    else
+        return False
