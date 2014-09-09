37aab9b3b03f4e5de029621087a8f4e5be60d30f
João Júnior <joaojunior_ma@gmail.com>
2014-09-09 20:49:59 -0300

    Adicionando a chamada do main

--- test.py
+++ test.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 #Resolvendo o problema do Ano Bissexto - http://dojopuzzles.com/problemas/exibe/ano-bissexto/
 
+import sys
 #import unittest
 #class TestAnoBissexto(unittest.TestCase):
     
@@ -11,5 +12,9 @@ def bissexto(ano):
     # verifica se divisivel por 400
     elif ano % 400 == 0:
         return True
-    else:
+    else
         return False
+
+if __name__ == '__main__':
+    ano = int(sys.argv[1])
+    bissexto(ano)
