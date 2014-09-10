85ff7c081c9875ed1457ec2da8c7dd42ef21a66f
João Júnior <joaojunior_ma@gmail.com>
2014-09-09 21:34:54 -0300

    Solução Final

--- test.py
+++ test.py
@@ -4,11 +4,25 @@
 import unittest
 
 def bissexto(ano):
-    return True
+    if (ano % 4 == 0 and ano % 100 != 0) or ano % 400 == 0:
+        return True
+    return False
 
 class TestAnoBissexto(unittest.TestCase):
-    def test_passa_multiplo_4(self):
+    def test_passa_multiplo_4_return_True(self):
         self.assertTrue(bissexto(1732))
+        
+    def test_ano_nao_bissexto_return_False(self):
+        self.assertFalse(bissexto(1742))
+        
+    def test_ano_100_return_False(self):
+        self.assertFalse(bissexto(100))
+        
+    def test_ano_1600_return_True(self):
+        self.assertTrue(bissexto(1600))
+        
+    def test_ano_0_return_True(self):
+        self.assertTrue(bissexto(0))
         
 if __name__ == "__main__":
     unittest.main()
