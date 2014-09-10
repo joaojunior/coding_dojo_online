5dbaff1384089de65e7eb18b5ed569bb3638cec8
João Júnior <joaojunior_ma@gmail.com>
2014-09-09 21:01:03 -0300

    Criando teste para verificar divisao por 4

--- test.py
+++ test.py
@@ -1,20 +1,10 @@
 # -*- coding: utf-8 -*-
 #Resolvendo o problema do Ano Bissexto - http://dojopuzzles.com/problemas/exibe/ano-bissexto/
 
-import sys
-#import unittest
-#class TestAnoBissexto(unittest.TestCase):
-    
-def bissexto(ano):
-    # verifica se o ano é multiplo de 4 e não divisível por 100
-    if ano % 4 == 0 and ano % 100 != 0:
-        return True
-    # verifica se divisivel por 400
-    elif ano % 400 == 0:
-        return True
-    else:
-        return False
-
-if __name__ == '__main__':
-    ano = int(sys.argv[1])
-    bissexto(ano)
+import unittest
+class TestAnoBissexto(unittest.TestCase):
+    def test_passa_multiplo_4(self):
+        self.assertTrue(bissexto(400))
+        
+if __name__ == "__main__":
+    unittest.main()
